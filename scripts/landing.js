ea24280f8f15f506e22e0c98abbc74163ecfa1d7
Brandon Ward <Brandon Ward>
2017-03-05 15:39:48 -0800

    Checkpoint 8 strange things happened, so I had to go back and fix

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
 var animatePoints = function() {
  
-<<<<<<< HEAD
-                 var points = document.getElementsByClassName('point');
+                var points = document.getElementsByClassName('point');
  
                  var revealFirstPoint = function() {
                      points[0].style.opacity = 1;
@@ -28,34 +27,5 @@ var animatePoints = function() {
                  revealSecondPoint();
                  revealThirdPoint();
  
-             };
-=======
-     var points = document.getElementsByClassName('point');
- 
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
- 
- };
->>>>>>> checkpoint-8-dom-scripting-animation
+};
+     
