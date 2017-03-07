9f5e765399860ff04f1bb71f9486eea382fbb50f
Brandon Ward <Brandon Ward>
2017-03-06 23:18:10 -0800

    Checkpoint 9 DOM Scripting Events Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,32 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
  
-                var points = document.getElementsByClassName('point');
+     var revealPoint = function(index) {
+         points[index].style.opacity = 1;
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+         }
+         for(var i = 0; i < points.length; i++) {
+             revealPoint(i);   
+         }
+ };  
+
+window.onload = function(){
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
-                var revealPoint = function(index) {
-                     points[index].style.opacity = 1;
-                     points[index].style.transform = "scaleX(1) translateY(0)";
-                     points[index].style.msTransform = "scaleX(1) translateY(0)";
-                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 }
-                for(var i = 0; i < points.length; i++) {
-                    revealPoint(i);   
-                 }
- };   
+    window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {           animatePoints(pointsArray); 
+        }
+    });
+}
+
+
+
+
+
