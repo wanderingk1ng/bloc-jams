6bc3f9fee5793cb73abac5f8a143d72c17dc601c
Brandon Ward <Brandon Ward>
2017-03-08 21:52:43 -0800

    Checkpoint 11 DOM scripting album view complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,13 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
+var animatePoints = function (points) {
  
-     var revealPoint = function(index) {
+     var revealPoint = function (index) {
          points[index].style.opacity = 1;
          points[index].style.transform = "scaleX(1) translateY(0)";
          points[index].style.msTransform = "scaleX(1) translateY(0)";
          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-         }
+            }
          for(var i = 0; i < points.length; i++) {
              revealPoint(i);   
          }
@@ -21,7 +21,8 @@ window.onload = function(){
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     window.addEventListener("scroll", function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {           animatePoints(pointsArray); 
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {  
+            animatePoints(pointsArray); 
         }
     });
 }
